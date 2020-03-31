b9dccab38a799c081dc02c25447b3a78f2472ac0
Giga487 <Giga487@hotmail.com>
2020-03-31 10:32:46 +0200

    Iris DB

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ import pandas as pd
 df1 = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data")
 
 df1.to_csv("autosCsv")
+
+df1 = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
+
+df1.to_csv("Iris")
